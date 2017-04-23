8daf6b0bc3aa51eb70df6562c654b8c8d1f74a46
Lingch <CyberLingch@gmail.com>
2017-04-23 22:57:29 +0800

    do save

--- myscript.js
+++ myscript.js
@@ -1,19 +1,31 @@
 //  
 $(document).ready(function() {
+	$(window).on('hashchange', urlChanged);
+	urlChanged();
+});
 
+function urlChanged(){
 	if (document.URL.indexOf("/dp/") < 0) {
 		return;
 	}
 
-	$('<a>').text("Save")
+	$("#mysavedinfo").remove();
+	$('<a>').attr("id","mysaveinfo").text("Save|")
 		.insertBefore($("#productTitle"))
 		.click(function() {
-			saveInfo();
+			saveInfo(function(status){
+				if(status == 200){
+					$("#mysaveinfo").remove();
+					$('<cpan>').attr("id","mysavedinfo").text("Saved|")
+					.insertBefore($("#productTitle"))
+				}
+			});
 		})
-});
+}
 
-function saveInfo() {
+function saveInfo(callback) {
 	var obj = {};
+	obj.asin = $("#averageCustomerReviews").attr("data-asin");
 	obj.title = $("#productTitle").text().trim();
 	obj.listPrice = $("#price > table > tbody > tr:nth-child(1) > td.a-span12.a-color-secondary.a-size-base > span").text().trim();
 	obj.price = $("#priceblock_ourprice").text().trim();
@@ -33,16 +45,14 @@ function saveInfo() {
 	
 	obj.detail = $.base64('encode', $("#aplus").html());
 
-var v = JSON.stringify(obj);
-
 	$.ajax({
 		type: "POST",
 		dataType: "json",
 		data: JSON.stringify(obj),
 		contentType: "application/json; charset=utf-8",
 		url: "http://192.168.2.202:3000/AMZItem",
-		success: function(data) {
-			alert(data);
+		complete: function(datacheck) {
+			callback(datacheck.status);
 		}
 	});
 }
